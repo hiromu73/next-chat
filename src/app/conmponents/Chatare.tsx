1e752463f0084fdcfddd9cf3ad017ac7eecb0538
hiromu73 <hiromu.camera@gmail.com>
2025-01-23 21:56:59 +0900

    更新:Chatmessageの修正

--- src/app/conmponents/Chatare.tsx
+++ src/app/conmponents/Chatare.tsx
@@ -13,7 +13,18 @@ const Chatare = () => {
   const { message, setMessage } = useChatContext();
   return test_1 ? (
     <Box sx={{ bgcolor: (theme) => theme.palette.background.paper, width: "100%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <Box sx={{ width: "70%", height: "80%", display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          width: "70%",
+          height: "80%",
+          display: "flex",
+          flexDirection: "column",
+          overflowY: "overlay",
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(0, 0, 0, 0.2)",
+          },
+        }}
+      >
         <Chatmessage message={message} />
       </Box>
       <Box sx={{ width: "60%", display: "flex", flexDirection: "column", position: "absolute", bottom: 10 }}>
--- src/app/conmponents/Chatmessage.tsx
+++ src/app/conmponents/Chatmessage.tsx
@@ -10,7 +10,7 @@ const Chatmessage = ({ message }: ChatMessageProps) => {
   console.log("chatmessage");
   console.log(message);
   return (
-    <Box sx={{ flex: 1, p: 1, width: "100%", maxWidth: "100%", height: "100%", alignSelf: "flex-start", overflow: "auto" }}>
+    <Box sx={{ width: "100%", height: "100%", alignSelf: "flex-start" }}>
       {message.map((msg, index) => (
         <ChatList key={index} message={msg} user={index % 2 === 0} />
       ))}
